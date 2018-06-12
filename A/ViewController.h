2a470b1b77793d10aa39cc73a5719812d27c71fb
GN_木头人 <946193683@.com>
2018-06-12 18:12:11 +0800

    1

---
+++ A/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A
+//
+//  Created by MGN on 2018/6/12.
+//  Copyright © 2018年 lib.rrslj.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ A/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A
+//
+//  Created by MGN on 2018/6/12.
+//  Copyright © 2018年 lib.rrslj.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
